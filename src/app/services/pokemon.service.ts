fe6c5ca1040fea14167d7d7c161727154155a15f
Victor Lima <victorhenriqu3@outlook>
2023-12-28 22:23:52 -0400

    feat: implements http requests for pokemon search

---
+++ src/app/services/pokemon.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { PokemonData } from '../models/pokemonData';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PokemonService {
+  private baseURL: string = '';
+  private pokeData: PokemonData | any;
+  constructor(private http: HttpClient) {
+    this.baseURL = environment.pokeApi;
+  }
+
+  getPokemon(pokemonName: string): Observable<PokemonData> {
+    this.pokeData = this.http.get<PokemonData>(`${this.baseURL}${pokemonName}`);
+    //console.log(this.pokeData);
+    return this.pokeData;
+  }
+}
